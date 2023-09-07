8e2570d77ddef689515aa58d7befba83ad100fbf
Yevhenii <yevhenii.bakhmat@nure.ua>
2023-09-07 17:20:02 +0300

    working on init command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct CLI {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init { path: Option<String> },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = CLI::parse();
+
+    match &cli.command {
+        None => println!("No command"),
+        Some(Commands::Init { path }) => {
+            println!("Init command with path: {:?}", path);
+        }
+    }
 }
