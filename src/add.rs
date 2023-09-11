8809a2790c44e8c11454f2b65c4a3279420beb19
JohnBakhmat <johnbakhmat@gmail.com>
2023-09-11 12:35:03 +0300

    Added glob for easier folder access

--- src/add.rs
+++ src/add.rs
@@ -1,15 +1,25 @@
+use git2::Repository;
+use glob::glob;
 use std::fs;
 use std::path::Path;
 
 pub async fn run(path: &str, lang: &str) -> anyhow::Result<()> {
-    let path = Path::new(path);
-    let template_dir = Path::new("/home/john/projects/aoc-cli/src/templates").join(lang);
+    let folders = glob(&format!("{}/**/day*", path))?
+        .map(|x| x.unwrap())
+        .collect::<Vec<_>>();
 
-    assert!(
-        template_dir.exists(),
-        "Sorry! We have no template for that language"
-    );
-    println!("{:?}", template_dir);
+    let already_exists = glob(&format!("{}/**/day*/{}", path, lang))?
+        .map(|x| x.unwrap())
+        .collect::<Vec<_>>();
+
+    let folders_to_add = folders
+        .iter()
+        .filter(|x| !already_exists.contains(x))
+        .collect::<Vec<_>>();
+
+    for folder in folders_to_add {
+        println!("Adding {} to {}", lang, folder.display());
+    }
 
     Ok(())
 }
